efe1ede0f5d9ce9ff86db15dc409c0b72cb216b0
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-02 16:17:37 +0200

    FEAT delete helloworld + add project

---
+++ src/components/Project.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: []
+    };
+  },
+  mounted() {
+  axios.get('http://127.0.0.1:8000/api/projects')
+    .then(response => {
+      this.projects = response.data.data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+};
+</script>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p class="card-text">{{ project.customer }}</p>
+        <p class="card-text">{{ project.description }}</p>
+        <p class="card-text">{{ project.url }}</p>
+        <p class="card-text">{{ project.status }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      project: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .card {
+    margin-bottom: 20px;
+  }
+  </style>
+  
