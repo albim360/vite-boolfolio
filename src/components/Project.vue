adc75b5d410ae36e7fff845f9aa5309f6b4818e9
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-02 16:22:55 +0200

    FEAT change metod + project.title

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
     </ul>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
@@ -15,14 +16,14 @@ export default {
       projects: []
     };
   },
-  mounted() {
-  axios.get('http://127.0.0.1:8000/api/projects')
-    .then(response => {
-      this.projects = response.data.data;
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+  created() {
+    axios.get('http://127.0.0.1:8000/api/projects')
+      .then(response => {
+        this.projects = response.data.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 </script>
