5baa87b33e825980d79ff2c3217aa715205d5865
Alberto <74522184+albim360@users.noreply.github.com>
2023-05-03 17:29:17 +0200

    FIX visual

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,36 +1,31 @@
 <template>
-    <div class="container">
-        <div class="row flex-row">
-            <div class="col-12">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title">Titolo: {{ project.title }}</h5>
-                    <p class="card-text">Cliente: {{ project.customer }}</p>
-                    <p class="card-text">Descrizione: {{ project.description }}</p>
-                    <p class="card-text">URL: {{ project.url }}</p>
-                    <p class="card-text">Stato: {{ project.status }}</p>
-                  </div>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Titolo: {{ project.title }}</h5>
+        <p class="card-text">Cliente: {{ project.customer }}</p>
+        <p class="card-text">Descrizione: {{ project.description }}</p>
+        <p class="card-text">URL: {{ project.url }}</p>
+        <p class="card-text">Stato: {{ project.status }}</p>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      project: {
-        type: Object,
-        required: true,
-      },
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true,
     },
-  };
-  </script>
-  
-  <style scoped>
-  .card {
-    margin-bottom: 20px;
-    width: 100%;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  margin-bottom: 20px;
+  width: 100%;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
 import ProjectCard from './components/ProjectCard.vue'
-
+import axios from 'axios';
 
 const routes = [
   {
@@ -12,8 +12,36 @@ const routes = [
   },
   {
     path: '/projects',
-    name: 'ProjectCard',
-    component: ProjectCard
+    name: 'Projects',
+    component: {
+      template: `
+        <div class="container">
+          <h1>Projects</h1>
+          <div class="row">
+            <div class="col-md-4" v-for="project in projects" :key="project.id">
+              <project-card :project="project"></project-card>
+            </div>
+          </div>
+        </div>
+      `,
+      components: {
+        'project-card': ProjectCard
+      },
+      data() {
+        return {
+          projects: []
+        }
+      },
+      mounted() {
+        axios.get('http://localhost:8000/api/projects')
+          .then(response => {
+            this.projects = response.data.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    }
   },
   {
     path: '/about',
